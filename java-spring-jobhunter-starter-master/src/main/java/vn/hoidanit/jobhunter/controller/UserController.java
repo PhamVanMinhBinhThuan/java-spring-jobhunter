9daacc148c1df19dd694f4fd93ea0aeee8fb1cf6
Pham Minh <phamminhpro1405@gmail.com>
2026-01-30 14:15:06 +0700

    feat(BE): Add exception gloabal

--- java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 import java.util.List;
 
@@ -31,7 +32,10 @@ public class UserController {
     }
 
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) throws IdInvalidException {
+        if (id >= 1500)
+            throw new IdInvalidException("Id khong lon hon 1500");
+
         this.userService.handleDeleteUser(id);
         return ResponseEntity.status(HttpStatus.OK).body("Deleted");
     }
