8daf9902864fd7593f6132c9109b5683e1d8849a
Pham Minh <phamminhpro1405@gmail.com>
2026-02-04 02:28:04 +0700

    feat(BE): Update Authentication

--- java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
+++ java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import vn.hoidanit.jobhunter.domain.dto.LoginDTO;
+import vn.hoidanit.jobhunter.domain.dto.ResLoginDTO;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,19 +25,21 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDto) {
         // Nạp input gồm username/password vào Security
-        UsernamePasswordAuthenticationToken authenticationToken 
-            = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
- 
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                loginDto.getUsername(), loginDto.getPassword());
+
         // Xác thực người dùng => cần viết hàm loadUserByUsername
         // authentication khong luu mat khau nguoi dung sau khi dang nhap thanh cong
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
         // Create a token
         // authentication khong luu mat khau nguoi dung sau khi dang nhap thanh cong
-        this.securityUtil.createToken(authentication);
+        String access_token = this.securityUtil.createToken(authentication);
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(access_token);
 
-        return ResponseEntity.ok().body(loginDto);
+        return ResponseEntity.ok().body(res);
     }
 }
