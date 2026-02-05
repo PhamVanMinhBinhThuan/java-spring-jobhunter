6526914666f927464da334c49b52d285f68e726d
Pham Minh <phamminhpro1405@gmail.com>
2026-02-05 17:30:18 +0700

    feat(BE): Add APIs for update / get /delete company

--- java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
+++ java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.Company;
@@ -16,5 +19,26 @@ public class CompanyService {
 
     public Company handleCreateCompany(Company c) {
         return this.companyRepository.save(c);
+    }
+
+    public List<Company> handleGetCompany() {
+        return this.companyRepository.findAll();
+    }
+
+    public Company handleUpdateCompany(Company c) {
+        Optional<Company> companyOptional = this.companyRepository.findById(c.getId());
+        if (companyOptional.isPresent()) {
+            Company currentCompany = companyOptional.get();
+            currentCompany.setName(c.getName());
+            currentCompany.setAddress(c.getAddress());
+            currentCompany.setDescription(c.getDescription());
+            currentCompany.setLogo(c.getLogo());
+            return this.companyRepository.save(currentCompany);
+        }
+        return null;
+    }
+
+    public void handleDeleteCompany(long id) {
+        this.companyRepository.deleteById(id);
     }
 }
