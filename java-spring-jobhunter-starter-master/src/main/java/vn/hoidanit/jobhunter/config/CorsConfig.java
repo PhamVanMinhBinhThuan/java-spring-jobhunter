477e96d0a77bf878c2d87dfd4eff8c680af69508
Pham Minh <phamminhpro1405@gmail.com>
2026-02-08 12:37:07 +0700

    fix(BE): Fix response API for frontend

--- java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/config/CorsConfig.java
+++ java-spring-jobhunter-starter-master/src/main/java/vn/hoidanit/jobhunter/config/CorsConfig.java
@@ -14,10 +14,20 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+
+        // Cho phép URL nào có thể tới nối tới Backend
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4173", "http://localhost:5173"));
+        
+        // Các method nào được kết nối
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
+        
+        // Các phần header được phép gửi lên
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
+        
+        // Gửi kèm cookie hay không
         configuration.setAllowCredentials(true);
+        
+        // Thời gian pre-light request có thể cache (tính theo seconds)
         configuration.setMaxAge(3600L);
         // How long the response from a pre-flight request can be cached by clients
 
